00f09127ddf8e38d56b28b1c9d48aa0c68426d74
Dipu K Raj <kumardipu1436@gmail.com>
2025-12-01 23:56:49 +0530

    update

--- script.js
+++ script.js
@@ -178,6 +178,13 @@ const songs = [
         cover: "new image/Bol Kaffara Kya Hoga.jpg",
         category: "new"
     },
+    {
+        title: " Deewani Mastani",
+        file: "new music/Deewani Mastani.mp3",
+        cover: "new image/Deewani Mastani.jpg",
+        category: "new"
+    },
+
     // Old Songs
     {
         title: "Lag Jaa Gale",
