5a34898a0242e5a9450c5a005ffb44a9d4d03516
Dipu K Raj <kumardipu1436@gmail.com>
2025-12-03 22:34:01 +0530

    update

--- script.js
+++ script.js
@@ -282,6 +282,12 @@ const songs = [
         cover: "old image/Ho Gaya Hai Tujhko.jpg",
         category: "old"
     },
+    {
+        title: " Ho Gaya Hai Tujhko To Pyar Sajna",
+        file: "old music/Ho Gaya Hai Tujhko.mp3",
+        cover: "old image/Ho Gaya Hai Tujhko.jpg",
+        category: "old"
+    },
 
     // Romantic Songs
     {
