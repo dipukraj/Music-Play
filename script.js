e26f21576776b0ce7c662572f59ddefbf2dbe548
Dipu K Raj <kumardipu1436@gmail.com>
2025-12-06 23:08:50 +0530

    update

--- script.js
+++ script.js
@@ -208,6 +208,12 @@ const songs = [
         cover: "new image/Deewani Mastani.jpg",
         category: "new"
     },
+    {
+        title: " Deewani Mastani",
+        file: "new music/Deewani Mastani.mp3",
+        cover: "new image/Deewani Mastani.jpg",
+        category: "new"
+    },
 
     // Old Songs
     {
